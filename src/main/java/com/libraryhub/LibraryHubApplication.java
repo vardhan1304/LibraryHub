6f60e28128a2ba545b701ad6f4ef46947a38bb5e
vardhan1304 <kirtivardhan8@gmail.com>
2026-02-04 16:51:39 +0530

    added

---
+++ src/main/java/com/libraryhub/LibraryHubApplication.java
@@ -0,0 +1,13 @@
+package com.libraryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libraryhub/LibraryHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
